ed88c9aea42994b1cbce94240dd1ed5bab970cfc
Jim Porter <jporter@mozilla.com>
2015-05-11 21:13:50 -0500

    Clean up the Giant Bomb API code

--- plugin.video.giantbomb/resources/lib/giantbomb.py
+++ plugin.video.giantbomb/resources/lib/giantbomb.py
@@ -2,11 +2,34 @@ import simplejson
 import urllib
 import urllib2
 
-API_PATH = 'https://www.giantbomb.com/api'
-DEFAULT_API_KEY = 'fa96542d69b4af7f31c2049ace5d89e84e225bef'
+class APIError(Exception):
+    """An error for when the Giant Bomb API doesn't like something we did."""
+
+    error_descs = {
+        100: 'Invalid API Key',
+        101: 'Object Not Found',
+        102: 'Error in URL Format',
+        103: "'jsonp' format requires a 'json_callback' argument",
+        104: 'Filter Error',
+        105: 'Subscriber only video is for subscribers only',
+        107: 'Rate limit exceeded',
+    }
+
+    def __init__(self, status):
+        """Create a new Giant Bomb API error.
+
+        :param status: The status code from the API response"""
+
+        Exception.__init__(self, self.error_descs.get(
+            status, 'Unknown Status Code {0}'.format(status)
+        ))
+        self.status = status
 
 class GiantBomb(object):
     """A simple interface to the Giant Bomb API."""
+
+    api_path = 'https://www.giantbomb.com/api'
+    default_api_key = 'fa96542d69b4af7f31c2049ace5d89e84e225bef'
 
     def __init__(self, api_key=None, on_update_api_key=None):
         """Create a new instance of the Giant Bomb API requester.
@@ -15,15 +38,15 @@ class GiantBomb(object):
         :param on_update_api_key: A function to call if the API key is changed
         """
 
-        self.api_key = api_key or DEFAULT_API_KEY
+        self.api_key = api_key or self.default_api_key
         self.on_update_api_key = on_update_api_key
 
     def get_api_key(self, link_code):
         """Get the API key from the site given the link code.
 
         :param link_code: The link code received from the Giant Bomb website
-        :return: The API key, or None if something went wrong
-        """
+        :return: The API key, or None if something went wrong"""
+
         if link_code and len(link_code) == 6:
             data = self.query('validate', { 'link_code': link_code })
             if 'api_key' in data:
@@ -47,13 +70,13 @@ class GiantBomb(object):
         if query:
             full_query.update(query)
         try:
-            return self._do_fetch(API_PATH + '/' + resource + '?' +
+            return self._do_fetch(self.api_path + '/' + resource + '?' +
                                   urllib.urlencode(full_query))
-        except Exception:
-            if retry:
+        except APIError as e:
+            if retry and e.status == 100:
+                self._reset_api_key()
                 return self.query(resource, query, format, retry=False)
-            else:
-                raise
+            raise
 
     def fetch(self, url, format='json', retry=True):
         """Fetch a pre-built URL from the Giant Bomb API.
@@ -66,11 +89,18 @@ class GiantBomb(object):
         query = { 'api_key': self.api_key, 'format': format }
         try:
             return self._do_fetch(url + '?' + urllib.urlencode(query))
-        except:
-            if retry:
+        except APIError as e:
+            if retry and e.status == 100:
+                self._reset_api_key()
                 return self.fetch(url, format, retry=False)
-            else:
-                raise
+            raise
+
+    def _reset_api_key(self):
+        """Reset the API key (e.g. when the API tells us our key is bad."""
+
+        self.api_key = self.default_api_key
+        if callable(self.on_update_api_key):
+            self.on_update_api_key(self.api_key)
 
     def _do_fetch(self, url):
         """Fetch a URL from the Giant Bomb API.
@@ -83,20 +113,4 @@ class GiantBomb(object):
 
         if status == 1:
             return data
-        else:
-            if status == 100:
-                self.api_key = DEFAULT_API_KEY
-                if callable(self.on_update_api_key):
-                    self.on_update_api_key(self.api_key)
-
-            error_descrs = {
-                100: 'Invalid API Key',
-                101: 'Object Not Found',
-                102: 'Error in URL Format',
-                103: "'jsonp' format requires a 'json_callback' argument",
-                104: 'Filter Error',
-                105: 'Subscriber only video is for subscribers only',
-                }
-            descr = error_descrs.get(status,
-                                     'Unknown Status Code {0}'.format(status))
-            raise Exception(descr)
+        raise APIError(status)
